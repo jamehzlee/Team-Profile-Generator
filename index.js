268c0c113c1673cf6a1f48fbb8575e6e2076689c
Jamehzz <probableeme@gmail.com>
2022-03-13 21:06:13 -0400

    added user prompt

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 
-fs.appendFile('./dist/index.html', data, (err) => 
-    err ? console.log(err) : console.log('Team created!')
-);
+const employee = new Employee();
+const manager = new Manager();
+const engineer = new Engineer();
+const intern = new Intern();
+
+manager.getRole();
