bbe8ab5c718783007902f59e4cbdf711c302ff8c
Jamehzz <probableeme@gmail.com>
2022-03-16 09:55:27 -0400

    added prompts for engineer and intern classes

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ console.log(
 );
 
 function appMenu() {
-  // Create manager first, then the manager will create a team
-  // Once manager is craeted, we will create team by asking the user which type of employee to create
-  // Based on the choice, we will create that employee object
-  // Loop throu the create team function until user is done from creating employees for the team
-  // then we will use the employee objects created to render html for the team
-
+  
   function createManager() {
     console.log('Please build your team 👥');
     inquirer
@@ -34,7 +29,7 @@ function appMenu() {
         {
           type: 'input',
           name: 'managerName',
-          message: "What is the team manager's name?",
+          message: "What is the Team Manager's name?",
           validate: (answer) => {
             if (answer !== '') {
               return true;
@@ -42,11 +37,33 @@ function appMenu() {
             return 'Please enter at least one character.';
           },
         },
-        //
-        // TODO: YOUR CODE HERE:
-        // CREATE REST OF QUESTIONS HERE FOR MANAGER
-        // Strongly recommend to add validate property function for id and email
-        //
+        {
+          type: 'input',
+          name: 'managerId',
+          message: "What is the Team Manager's ID?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'managerEmail',
+          message: "What is the Team Manager's email?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'managerOfficeNumber',
+          message: "What is the Team Manager's office number?",
+        }
       ])
       .then((answers) => {
         const manager = new Manager(
@@ -55,8 +72,8 @@ function appMenu() {
           answers.managerEmail,
           answers.managerOfficeNumber
         );
-        teamMembers.push(manager);
         idArray.push(answers.managerId);
+        teamMembers.push(manager);
         createTeam();
       });
   }
@@ -92,20 +109,60 @@ function appMenu() {
   function addEngineer() {
     inquirer
       .prompt([
-        //
-        // TODO: YOUR CODE HERE
-        // CREATE OBJECTS OF QUESTIONS FOR ENGINEER
-        //
+        {
+          type: 'input',
+          name: 'engineerName',
+          message: "What is the Engineer's name?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerId',
+          message: "What is the Engineer's ID?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerEmail',
+          message: "What is the Engineer's email?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerGithub',
+          message: "What is the Engineer's GitHub username?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        }
       ])
       .then((answers) => {
-        //
-        // TODO: YOUR CYOUR CODE HERE
-        // 1. CREATE A VARIABLE TO STORE THE ENGINEER OBJECT INSTANTIATED WITH THE ENGINEER CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS
-        //    TO THE ENGINEER CLASS CONSTRUCTOR
-        // 2. ADD (PUSH) THE ENGINEER VARIABLE TO the teamMembers ARRAY
-        // 3. ADD (PUSH) THE ENGINERR ID TO THE idArray ARRAY
-        //
-
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        idArray.push(answers.engineerId);
+        teamMembers.push(engineer);
         createTeam();
       });
   }
@@ -113,19 +170,60 @@ function appMenu() {
   function addIntern() {
     inquirer
       .prompt([
-        //
-        // TODO: YOUR CODE HERE
-        // CREATE OBJECTS OF QUESTIONS FOR INTERN
-        //
+        {
+          type: 'input',
+          name: 'internName',
+          message: "What is the Intern's name?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'internId',
+          message: "What is the Intern's ID?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'internEmail',
+          message: "What is the Intern's email?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'internSchool',
+          message: "What is the Intern's school?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        }
       ])
       .then((answers) => {
-        //
-        // TODO: YOUR CODE HERE
-        // 1. CREATE A VARIABLE TO STORE THE INTERN OBJECT INSTANTIATED WITH THE INTERN CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS
-        //    TO THE INTERN CLASS CONSTRUCTOR
-        // 2. ADD (PUSH) THE INTERN VARIABLE TO the teamMembers ARRAY
-        // 3. ADD (PUSH) THE INTERN ID TO THE idArray ARRAY
-        //
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        idArray.push(answers.internId);
+        teamMembers.push(intern);
         createTeam();
       });
   }
